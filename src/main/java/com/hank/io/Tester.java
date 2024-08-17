257581f8c9ea6728de371116627bb33200df0473
DannyLien <danny.lien1109@gmail.com>
2024-08-17 22:25:42 +0800

    7-3, Java FileUnputStream Read data.txt;

--- src/main/java/com/hank/io/Tester.java
+++ src/main/java/com/hank/io/Tester.java
@@ -3,19 +3,25 @@ package com.hank.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Tester {
     public static void main(String[] args) {
         File file = new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
-        try{
+        try {
             FileInputStream is = new FileInputStream(file);
-
-        }
-        catch (FileNotFoundException e){
-            e.printStackTrace();
-            System.out.println("message");
+            int n = is.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = is.read();
+            }
+            System.out.println();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         System.out.println("File optn success");
     }
