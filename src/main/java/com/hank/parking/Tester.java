bfd960db71b2fc8094c744e0f22a4182891f298d
DannyLien <danny.lien1109@gmail.com>
2024-08-19 14:20:27 +0800

    9-1, Java日期 Date Calendar 案例;

---
+++ src/main/java/com/hank/parking/Tester.java
@@ -0,0 +1,36 @@
+package com.hank.parking;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        //java8
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getTime());
+        SimpleDateFormat sdf =
+                new SimpleDateFormat("yyy/MM/dd HH:mm:ss");
+        System.out.println(sdf.format(date));
+        String s = "2018/11/25 08:00:00";
+        Date other = null;
+        try {
+            other = sdf.parse(s);
+            System.out.println(other);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        calendar.set(Calendar.MONTH,9);
+        System.out.println(calendar.getTime());
+        calendar.add(Calendar.DAY_OF_YEAR,3);
+        System.out.println(calendar.getTime());
+
+    }
+
+
+}
