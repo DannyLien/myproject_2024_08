8a455748e997f41e58f2a17e1063d8e239253b19
DannyLien <danny.lien1109@gmail.com>
2024-08-19 14:31:32 +0800

    9-2, java8 新版 Instant 跟 LocalDateTime 時間日期類別;

--- src/main/java/com/hank/parking/Tester.java
+++ src/main/java/com/hank/parking/Tester.java
@@ -2,13 +2,38 @@ package com.hank.parking;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Tester {
 
     public static void main(String[] args) {
-        //java8
+
+
+//        java8();
+//        java();
+    }
+
+    private static void java8() {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+        //local
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.println(formatter.format(now));
+        System.out.println(now.plus(Duration.ofHours(3)));
+        LocalDateTime other =
+                LocalDateTime.of(2018,11,23,8,0,0);
+        System.out.println(other);
+    }
+
+    private static void java() {
         Date date = new Date();
         System.out.println(date);
         System.out.println(date.getTime());
@@ -29,7 +54,6 @@ public class Tester {
         System.out.println(calendar.getTime());
         calendar.add(Calendar.DAY_OF_YEAR,3);
         System.out.println(calendar.getTime());
-
     }
 
 
