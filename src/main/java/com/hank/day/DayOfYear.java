42f13f9099364a4e603d0ac682d0a0d5d4eed58f
DannyLien <danny.lien1109@gmail.com>
2024-08-19 10:20:51 +0800

    8-2, Java Calendar應用;

--- src/main/java/com/hank/day/DayOfYear.java
+++ src/main/java/com/hank/day/DayOfYear.java
@@ -3,6 +3,7 @@ package com.hank.day;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class DayOfYear {
@@ -10,11 +11,16 @@ public class DayOfYear {
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //        int year = scanner.nextInt();
+
+        Calendar now = Calendar.getInstance();
+        int year = now.get(Calendar.YEAR);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             System.out.println("Please enter year:");
             String yearString = reader.readLine();
-            int year = Integer.parseInt(yearString);
+            if (yearString.length() > 0) {
+                year = Integer.parseInt(yearString);
+            }
             System.out.println(year);
 
         } catch (IOException e) {
